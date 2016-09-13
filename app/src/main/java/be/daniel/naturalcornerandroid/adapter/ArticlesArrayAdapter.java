e12e2fbbc28c727401f1410b04b3b7d758d722f6
userdanydan <userdany.dan1952@laposte.net>
2016-09-13 12:05:41 +0200

    First commit and push

---
+++ app/src/main/java/be/daniel/naturalcornerandroid/adapter/ArticlesArrayAdapter.java
@@ -0,0 +1,40 @@
+package be.daniel.naturalcornerandroid.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import be.daniel.naturalcornerandroid.R;
+import be.daniel.naturalcornerandroid.model.Article;
+
+/**
+ * Created by ivymike on 12/09/16.
+ */
+public class ArticlesArrayAdapter extends ArrayAdapter<Article> {
+    private final Context context;
+    private final List<Article> values;
+    public ArticlesArrayAdapter(Context context, List<Article> objects) {
+        super(context, android.R.layout.simple_list_item_activated_1, objects);
+        this.context=context;
+        this.values= objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.item_layout, parent, false);
+        TextView tv = (TextView) rowView.findViewById(R.id.tv_item_row);
+        TextView tvPrice = (TextView) rowView.findViewById(R.id.tv_item_price_row);
+        ImageView iv = (ImageView) rowView.findViewById(R.id.image_article);
+        tv.setText(values.get(position).getDenomination());
+        tvPrice.setText(" EUR : " + values.get(position).getPrixUnitaire());
+        return rowView;
+    }
+}
---
+++ app/src/main/java/be/daniel/naturalcornerandroid/task/LoadBitmapTask.java
@@ -0,0 +1,14 @@
+package be.daniel.naturalcornerandroid.task;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+/**
+ * Created by ivymike on 13/09/16.
+ */
+public class LoadBitmapTask extends AsyncTask<String, Void, Bitmap> {
+    @Override
+    protected Bitmap doInBackground(String... params) {
+        return null;
+    }
+}
